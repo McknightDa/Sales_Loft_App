c576e9d60898523a8f976e826057ab22c4e8081c
Daniel Mcknight <38105166+McknightDa@users.noreply.github.com>
2018-11-09 16:42:27 -0500

    Edited People Controller
    Edited the people controller to be used.

---
+++ app/api/people_controller.rb
@@ -0,0 +1,14 @@
+class Api::PeopleController < ApplicationController
+   
+    def index
+        @people = People.all params[:page], params[:per_page]
+        render json: @people
+      end
+    
+      def show
+        @person = People.findById params[:id]
+        render json: @person
+      end
+
+
+end

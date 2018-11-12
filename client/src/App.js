198da0bc41e7eadc86fb420f8616f6d3b472c563
Daniel Mcknight <38105166+McknightDa@users.noreply.github.com>
2018-11-12 15:55:52 -0500

    Failing to Compile
    Struggling to make the proper api call. Pushing to not lose progress.

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
-
+import axios from 'axios'
 
 class App extends Component {
+
+
+  state = {
+    people: []
+  };
+    
+    componentDidMount(){
+      
+      axios.get(
+      url= 'https://api.salesloft.com/v2/people.json',
+      headers= 
+       { 'Postman-Token': '15a70771-f991-43a1-a5b6-18ca2a4786bf',
+         'cache-control': 'no-cache',
+        
+           // Step 5: Save the response array to this.state.people
+           .then((response) => {
+             const people = response.data.people;
+             this.setState({people: people});
+           })
+           .catch((error) => {
+             console.error("Error: ", error);
+           
+          },
+        
+
   render() {
     return (
       <div>
